3264b46669201d660eb47a09c6919522778b5cce
pinfada <cali77@hotmail.fr>
2025-05-28 18:45:31 +0200

    Ajout MessageBusProvider

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import { OrganismProvider } from './hooks/useOrganism';
+import { MessageBusProvider } from './hooks/MessageBusContext';
 
 const root = createRoot(document.getElementById('root')!);
 root.render(
-  <OrganismProvider>
-    <App />
-  </OrganismProvider>
+  <MessageBusProvider>
+    <OrganismProvider>
+      <App />
+    </OrganismProvider>
+  </MessageBusProvider>
 );
